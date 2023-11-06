a9ef341505c9f9cf1ba609c24fad0780d82160db
yasangi1 <111675421+yasangi1@users.noreply.github.com>
2023-11-06 10:49:36 +0100

    Update min_value.py

--- min_value.py
+++ min_value.py
@@ -2,13 +2,12 @@
 
 student_heights = input("Enter students heights seperated by space:   ").split()
 
-min_value = student_heights[0]
 for n in range(0, len(student_heights)-1):
-    #print(int(height))
-    if int(min_value) < int(student_heights[n+1]):
-        min_value = min_value
-    elif int(min_value) > int(student_heights[n+1]):
-        min_value = student_heights[n+1]
-    elif int(student_heights[n]) == int(student_heights[n+1]):
-        min_value = min_value
-print(min_value)
+ student_heights[n] = int(student_heights[n])
+ 
+min_height = student_heights[0]
+for height in student_heights:
+ if min_height > int(height):
+     min_height = height
+     
+print(f"The highest score in the class is: {min_height}")
