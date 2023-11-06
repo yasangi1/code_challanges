16a25d56b46c2418d463da82166607b7a65b5d34
yasangi1 <111675421+yasangi1@users.noreply.github.com>
2023-11-06 10:45:50 +0100

    Update max_value.py

--- max_value.py
+++ max_value.py
@@ -1,17 +1,13 @@
 # This is a program to find the maximum number of a list.
 
-student_scores = input("Please enter student scores:  ").split()
-max_value = student_scores[0]
+student_heights = input("Enter students heights seperated by space:   ").split()
 
-for n in range(0, len(student_scores)):
-  student_scores[n] = int(student_scores[n])
- # print(n)
-
-# Write your code below this row 👇
-  if int(max_value) > student_scores[n]:
-    max_value = max_value
-  elif int(max_value) < student_scores[n]:
-    max_value = student_scores[n]
-  elif int(max_value) == student_scores[n]:
-    max_value = max_value
+for n in range(0, len(student_heights)-1):
+ student_heights[n] = int(student_heights[n])
+ 
+max_height = 0
+for height in student_heights:
+ if max_height < int(height):
+     max_height = height
+     
 print(f"The highest score in the class is: {max_value}")
